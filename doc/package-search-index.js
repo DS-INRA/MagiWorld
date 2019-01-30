aa11d81c76ed6b9d160ed0836061d8d24f5a97ad
DS-INRA <vVt5kmpReVzblA50kE9k>
2019-01-30 16:25:09 +0100

    Added JavaDoc & updated UML diagram (doc/schema/)

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.magiworld"},{"l":"com.magiworld.characters"},{"l":"com.magiworld.exceptions"},{"l":"com.magiworld.moves.basic"},{"l":"com.magiworld.moves.special"}]
